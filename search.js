19f1e530e946ec2a71f52866660c337a29ed2e3b
Puki99 <puvaca@protonmail.ch>
2022-03-30 20:04:39 +0200

    Search bar done

---
+++ search.js
@@ -0,0 +1,17 @@
+const searchBtn=document.getElementById("Search-btn");
+
+
+const searchInput = document.querySelector(".search-input");
+console.log(searchBtn, searchInput);
+
+searchBtn.addEventListener('click', function(){
+    //searchInput.classList.add('active-search');
+    searchInput.classList.toggle('active-search');
+    console.log(searchInput);
+});
+
+
+document.querySelector('main').addEventListener('click',()=>{
+    searchInput.classList.remove("active-search");
+});
+//could add (if the window is resized the search bar should also remove "active-search")
